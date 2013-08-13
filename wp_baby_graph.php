16ea4bea4c882d14209c2ce2f58504998fed556f
Arnaud Banvillet <arnaud.banvillet@gmail.com>
2013-08-13 14:33:14 +0200

    Create wp_baby_graph.php
    Add the plugin header

---
+++ wp_baby_graph.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Wp Baby Graph
+Plugin URI: http://arnaudban.me
+Description: Show the evolution of your baby
+Version: 0.1
+Author: ArnaudBan
+Author URI: http://arnaudban.me
+License: GPL2
+*/
