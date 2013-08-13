5c55f3a200dd05f86fdaa76460bdde815da25769
arnaudban <abanvillet@eluere.com>
2013-08-13 15:56:52 +0200

    update custom poste type baby_stage
    Add metabox

--- wp_baby_graph.php
+++ wp_baby_graph.php
@@ -11,37 +11,89 @@ License: GPL2
 
 function abwbg_register_baby_stage() {
   $labels = array(
-    'name' => 'Books',
-    'singular_name' => 'Book',
-    'add_new' => 'Add New',
-    'add_new_item' => 'Add New Book',
-    'edit_item' => 'Edit Book',
-    'new_item' => 'New Book',
-    'all_items' => 'All Books',
-    'view_item' => 'View Book',
-    'search_items' => 'Search Books',
-    'not_found' =>  'No books found',
-    'not_found_in_trash' => 'No books found in Trash', 
-    'parent_item_colon' => '',
-    'menu_name' => 'Books'
+    'name'                => __('Baby stages', 'baby_graph'),
+    'singular_name'       => __('Baby stage', 'baby_graph'),
+    'add_new'             => __('Add New', 'baby_graph'),
+    'add_new_item'        => __('Add New Baby stage', 'baby_graph'),
+    'edit_item'           => __('Edit Baby stage', 'baby_graph'),
+    'new_item'            => __('New Baby stage', 'baby_graph'),
+    'all_items'           => __('All Baby stages', 'baby_graph'),
+    'view_item'           => __('View Baby stage', 'baby_graph'),
+    'search_items'        => __('Search Baby stages', 'baby_graph'),
+    'not_found'           => __('No Baby stages found', 'baby_graph'),
+    'not_found_in_trash'  => __('No Baby stages found in Trash', 'baby_graph'),
+    'parent_item_colon'   => __('', 'baby_graph'),
+    'menu_name'           => __('Baby stages', 'baby_graph'),
   );
 
   $args = array(
-  	'label'              => 'Baby stages'
+    'label'              => 'Baby stages',
     'labels'             => $labels,
-    'description'        => __('Add new baby measure associate to a date', 'baby_graph'),
+    'description'        => __('Add new baby measures associate to a date', 'baby_graph'),
     'exclude_from_search'=> true,
     'publicly_queryable' => false,
-    'show_ui'            => true, 
+    'show_ui'            => true,
     'show_in_nav_menus'  => false,
-    'show_in_menu'       => 'import.php', 
     'query_var'          => 'false',
     'capability_type'    => 'post',
-    'has_archive'        => false, 
+    'has_archive'        => false,
     'hierarchical'       => false,
-    'supports' => array( 'title', 'thumbnail', 'excerpt' )
-  ); 
+    'supports'           => array( 'title', 'excerpt' ),
+    'register_meta_box_cb' => 'abwbg_add_baby_stage_metabox',
+  );
 
   register_post_type( 'baby_stage', $args );
 }
 add_action( 'init', 'abwbg_register_baby_stage' );
+
+function abwbg_add_baby_stage_metabox(){
+  add_meta_box( 'abwbg_baby_stage_metabox', 'Baby Measures', 'abwbg_baby_stage_metabox_content', 'baby_stage' );
+}
+
+
+function abwbg_baby_stage_metabox_content( $post ){
+
+  // Use nonce for verification
+  wp_nonce_field( plugin_basename( __FILE__ ), 'abwbg_baby_stage_metabox' );
+
+  // The actual fields for data entry
+  $baby_measures = get_post_meta( $post->ID, 'abwbg_baby_measures', true );
+
+  ?>
+  <table class="form-table">
+    <tr>
+      <td><label for="abwbg_baby_weight"><?php _e('Weight', 'baby_stage' )?></label></td>
+      <td><input type="number" step="0.001" id="abwbg_baby_weight" name="abwbg_baby_measures[weight]" value="<?php if( isset($baby_measures['weight']) ) esc_attr_e($baby_measures['weight']) ?>" /></td>
+    </tr>
+    <tr>
+      <td><label for="abwbg_baby_size"><?php _e('Size', 'baby_stage' )?></label></td>
+      <td><input type="number" step="0.05" id="abwbg_baby_size" name="abwbg_baby_measures[size]" value="<?php if( isset($baby_measures['size']) ) esc_attr_e($baby_measures['size']) ?>" /></td>
+    </tr>
+    <tr>
+      <td><label for="abwbg_baby_head_circumference"><?php _e('Head Circumference', 'baby_stage' )?></label></td>
+      <td><input type="number" step="0.05" id="abwbg_baby_head_circumference" name="abwbg_baby_measures[head_circumference]" value="<?php if( isset($baby_measures['head_circumference']) ) esc_attr_e($baby_measures['head_circumference']) ?>" /></td>
+    </tr>
+  </table>
+  <?php
+}
+
+function abwbg_save_baby_stage_meta( $post_id ){
+
+  if ( ! current_user_can( 'edit_post', $post_id ) )
+    return;
+
+
+  // Secondly we need to check if the user intended to change this value.
+  if ( ! isset( $_POST['abwbg_baby_stage_metabox'] ) || ! wp_verify_nonce( $_POST['abwbg_baby_stage_metabox'], plugin_basename( __FILE__ ) ) )
+      return;
+
+  // Sanitize user input
+  foreach ( $_POST['abwbg_baby_measures'] as $key => $num) {
+    if( is_numeric($num) )
+      $baby_measures[$key] = (float) $num;
+  }
+
+  update_post_meta($post_id, 'abwbg_baby_measures', $baby_measures );
+
+}
+add_action( 'save_post', 'abwbg_save_baby_stage_meta' );
